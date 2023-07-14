66973e04288b7ca76ff1bbf991e508a12ea05248
Alexey <kalnitskiy2018@mail.ru>
2023-07-14 22:23:06 +0500

    Упражнение 1

---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100 || num > 999){
+System.Console.Write("Число не является трехзначным");
+}   
+else{
+    num = num % 100;
+    double numD = num / 10;
+    Math.Floor(numD);
+    System.Console.WriteLine(numD);
+}
+
+
+
