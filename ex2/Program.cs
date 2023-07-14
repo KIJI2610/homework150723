5990aa26bc70ced813ed00c217bf279dc885fd93
Alexey <kalnitskiy2018@mail.ru>
2023-07-14 23:28:54 +0500

    Упражнение 2

---
+++ ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >= 100){
+    string numSrt = num.ToString();
+    char result = numSrt[2];
+    System.Console.WriteLine(result);
+}
+else{
+    System.Console.WriteLine("Нет третьей цифры");
+}
+
+
+
+
+
+
+
+
+
+
+
+// int nums = 32121;
+// string numsStr = nums.ToString();
+// char num = numsStr[2];
+// System.Console.WriteLine(num);
