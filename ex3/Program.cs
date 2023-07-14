05e6c5a180d42f058c68049795830fbcbd88dc3e
Alexey <kalnitskiy2018@mail.ru>
2023-07-14 23:40:36 +0500

    Упражнение 3

---
+++ ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day == 6 || day == 7){
+    System.Console.WriteLine("Это выходной");
+}
+else if (day >= 1 && day <= 5){
+    System.Console.WriteLine("Это будний день");
+}
+else{
+    System.Console.WriteLine("Введенное число не соответствует формату");
+}
+
+
